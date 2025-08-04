5253ea7f246e6ee4fde1ba5e450cdf3fffc534e6
Alvans <achicallag@unjbg.edu.pe>
2025-08-03 22:32:36 -0500

    agregando ejercicio 1 PlusOrMinus

---
+++ 1PlusOrMinus/PlusOrMinus.cpp
@@ -0,0 +1,26 @@
+/*
+A.Plus or Minus
+https://codeforces.com/problemset/problem/1807/A
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int a,b,c,cantidad;
+	cin>>cantidad;
+	while(cantidad--){
+		cin>>a>>b; 
+		c=a+b;
+		if((a+b)==c){
+			cout<<"+"<<endl;
+		}else{
+			cout<<"-"<<endl;
+		}
+		
+	}
+	
+	return 0;
+}
