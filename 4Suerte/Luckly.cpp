2945494bd2df7fa89901d9cbc8c4315c185b7993
Alvans <achicallag@unjbg.edu.pe>
2025-08-04 21:16:24 -0500

    agregando cuarto ejercicio de la suerte

---
+++ 4Suerte/Luckly.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int cantidad;cin>>cantidad;
+	//vector<int> suma1;
+	//vector<int> suma2;
+	
+	string numero;
+	while(cantidad--){
+		int suma1=0,suma2=0;
+		cin>>numero;
+		for(int i=0;i<3;i++){
+			int n1=(numero[i]-'0');
+			suma1+=n1;
+		}
+		for(int i=3;i<6;i++){
+			int n2=(numero[i]-'0');
+			//suma2.push_back(n2);
+			suma2+=n2;
+		}
+		if(suma1==suma2){
+			cout<<"YES"<<endl;
+		}else{
+			cout<<"NO"<<endl;
+		}
+		
+	}
+	
+	return 0;
+}
