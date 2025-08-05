59c0fe79515d2c41f756058cb7250f906e76a422
Alvans <achicallag@unjbg.edu.pe>
2025-08-04 22:19:30 -0500

    agregando ejercicio de otra forma

---
+++ 5Checking/1Prototipo.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int cantidad;cin>>cantidad;
+	while(cantidad--){
+		char c;cin>>c;
+		if(c=='c'||c=='o'||c=='d'||c=='e'||c=='f'||c=='o'||c=='r'||c=='c'||c=='e'||c=='s') cout<<"YES";
+		else cout<<"NO";
+		cout<<"\n";
+	}
+	return 0;
+}
