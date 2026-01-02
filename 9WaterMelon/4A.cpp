75cb7bfd5fd2e92036003617402934b3317beaa7
Alexander <achicallag@unjbg.edu.pe>
2026-01-02 00:19:00 -0500

    Add files via upload

---
+++ 9WaterMelon/4A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    int peso;cin>>peso;
+    if(peso % 2 == 0 && peso > 2){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+}
