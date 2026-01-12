07452ffd511b8ed4d86480260798394b8fa308d3
Alexander <achicallag@unjbg.edu.pe>
2026-01-11 23:15:58 -0500

    800

---
+++ 1030A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a;cin>>a;
+	int cen=0;
+	while(a--){
+		int b;cin>>b;
+		if(b==1){
+			
+			cen=1;
+		}
+	}
+	if(cen==0){
+		cout<<"EASY";
+	}else{
+		cout<<"HARD";
+	}
+	return 0;
+}
