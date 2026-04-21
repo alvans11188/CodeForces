0e40c4253756fdc2ded4bd238b582d279d6aecd4
Alexander <sander11199@gmail.com>
2026-04-21 12:02:10 -0500

    900

---
+++ 977Bv1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n,maximum=0;cin>>n;
+	char c,last;
+	string maximums,curr;
+	unordered_map<string,int>check;
+	for(int i=0;i<n;i++){
+		cin>>c;
+		if(i){
+			curr="";
+			curr+=last;
+			curr+=c;
+			check[curr]++;
+			if(check[curr]>maximum){
+				maximums=curr;
+				maximum=check[curr];
+			}
+		}
+		last=c;
+	}
+	cout<<maximums;
+	return 0;
+}
